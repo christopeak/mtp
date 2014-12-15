1d4d7d0d5d2cbb3da7be24d035e10daee349e119
Chris Peak <christopeak@gmail.com>
2014-12-14 17:06:01 -0800

    added three validation rules to application

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,7 @@
 class Application < ActiveRecord::Base
+
+	validates :projid, :title, :cost, :location, presence: true
+	validates :cost, numericality: {greater_than_or_equal_to: 0.01}
+	validates :cost, numericality: {less_than_or_equal_to: 9999999.99}
+
 end
