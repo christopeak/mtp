0e0f0c72e949fef2ef7544eddc249b7a390e7482
Chris Peak <christopeak@gmail.com>
2014-12-20 07:46:28 -0800

    slight change to css

--- test/models/application_test.rb
+++ test/models/application_test.rb
@@ -5,5 +5,30 @@ class ApplicationTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+	test "application attributes must not be empty" do
+		appform = Application.new
+		assert appform.invalid?
+		assert appform.errors[:projid].any?
+		assert appform.errors[:title].any?
+		assert appform.errors[:cost].any?
+		assert appform.errors[:location].any?
+	end
 	
+	test "project cost must be greater than $0.01" do
+		appform = Application.new(projid:		100032,
+															title:		"project 100032",
+															cost: 		0,
+															location:	"somewhere")
+		assert appform.invalid?
+		assert_equal ["must be greater than or equal to 0.01"],
+			appform.errors[:cost]
+		
+		appform.cost = -1
+		assert_equal ["must be greater than or equal to 0.01"],
+			appform.errors[:cost]
+			
+		appform.cost = 1
+		assert appform.valid?
+		
+	end
 end
